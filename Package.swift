74a503d422f73b54763a91c41444899ff0d4310e
Raffi <raffi@Raffis-MacBook-Pro.local>
2023-08-28 17:25:34 +0200

    add googlecast binary target and framework as dependency

--- Package.swift
+++ Package.swift
@@ -5,16 +5,24 @@ import PackageDescription
 let package = Package(
     name: "THEOplayerSDK",
     platforms: [
-        .iOS( .v11),
-        .tvOS(.v11)
+        .iOS( .v12),
+        .tvOS(.v12)
     ],
     products: [
         .library(name: "THEOplayerSDK", targets: ["THEOplayerSDK"]),
+        .library(name: "THEOplayerGoogleCastIntegration", targets: ["THEOplayerGoogleCastIntegration"])
+    ],
+    dependencies: [
+        .package(name: "GoogleCast", url: "https://github.com/Danesz/google-cast-sdk-dynamic-xcframework", from: "4.7.1")
     ],
     targets: [
         .binaryTarget(
             name: "THEOplayerSDK",
             path: "THEOplayerSDK-core/THEOplayerSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "THEOplayerGoogleCastIntegration",
+            path: "THEOplayer-Integration-GoogleCast/THEOplayerGoogleCastIntegration.xcframework"
         )
     ]
 )
